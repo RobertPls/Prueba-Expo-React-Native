ce130f6b3be9deea4ca86399e309012c61130cad
Robert Perez Lino <robertperezlino@gmail.com>
2022-05-17 12:12:14 -0400

    1.35 hrs USANDO REACT NAVIGATION*

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { TouchableNativeFeedback, TouchableWithoutFeedback } from 'react-native-web';
 import Main from './src/components/Main'
+import {NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <Main />
+    <NavigationContainer >
+      <Main />
+    </NavigationContainer >
   );
 }
